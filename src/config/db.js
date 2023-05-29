d6195ff4332b62916508a73dc8158a7b6ecde9ea
brunasdejesus <Bruna.Soares-ext2@ab-Inbev.com>
2023-05-28 22:20:50 -0300

    Atualização do arquivo db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,8 @@ const { Pool } = require('pg');
 
 module.exports = new Pool({
   user: 'postgres',
-  password: 'senha',
+  password: 'QAenginner@302',
   host: 'localhost',
   port: 5432,
-  database: 'launchstore'
+  database: 'Loja Virtual - Experimento'
 });
